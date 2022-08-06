09d8053238aa86554126b3f05ba9a816a37170e1
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-08-06 17:14:15 -0400

    cleaned up comments

--- main.js
+++ main.js
@@ -157,23 +157,4 @@ function awardPoints(checkAnswer, confirmAnswer, possiblePoints) {
     }
 }
 
-//////////////////////////////////////////////////////////////////
-//fetch template
 
-// document.querySelector('button').addEventListener('click', getFetch)
-
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-//   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
-
-//   fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data.subclasses)
-        
-
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
