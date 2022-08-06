4588088998ee3e21b0569c4e373849007dcfdd62
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-08-06 15:41:39 -0400

    added functions to check and award points, and to reset game

--- main.js
+++ main.js
@@ -47,6 +47,10 @@ let catArray = []
 
 function buildCategories() {
 
+    if (!(document.getElementById('category-row').firstChild.innerText == '')) {
+        resetBoard()
+    }
+
     const fetchReq1 = fetch(
         `https://jservice.io/api/category?&id=${randInt()}`
     ).then(res => res.json())   
@@ -80,6 +84,22 @@ function buildCategories() {
     })   
 }
 
+//RESET BOARD AND DOLLAR AMOUNTS IF NEEDED
+
+function resetBoard() {
+    let clueParent = document.getElementById('clue-board') 
+    while (clueParent.firstChild) {
+        clueParent.removeChild(clueParent.firstChild)
+    }
+    let catParent = document.getElementById('category-row')
+    while (catParent.firstChild) {
+        catParent.removeChild(catParent.firstChild)
+    }
+    document.getElementById('score').innerText = 0
+    initBoard()
+    initCatRow()
+}
+
 //LOAD CATEGORIES TO THE BOARD
 function setCategories(catArray) {
     let element = document.getElementById('category-row')
@@ -107,17 +127,35 @@ function getClue(event) {
 
 
 //SHOW QUESTION TO USER AND GET THEIR ANSWER
-
 function showQuestion(clue, target, boxValue) {
     let userAnswer = prompt(clue.question).toLowerCase()
     let correctAnswer = clue.answer.toLowerCase().replace(/<\/?[^>]+(>|$)/g, "") //regex voodoo
     let possiblePoints = +(boxValue)
     target.innerHTML = clue.answer
     target.removeEventListener('click', getClue, false)
+    evaluateAnswer(userAnswer, correctAnswer, possiblePoints)
 }
 
+//EVALUATE ANSWER AND SHOW TO USER TO CONFIRM
+function evaluateAnswer(userAnswer, correctAnswer, possiblePoints) {
+    let checkAnswer = (userAnswer == correctAnswer) ? 'correct' : 'incorrect'
+    let confirmAnswer = confirm(`Self check! For $${possiblePoints}, you answered "${userAnswer}", and the correct answer is "${correctAnswer}." Your answer appears to be ${checkAnswer}. Click OK to accept this decision, or click Cancel if the answer was not properly evaluated.`)
+    awardPoints(checkAnswer, confirmAnswer, possiblePoints) 
+}
 
+//AWARD POINTS
 
+function awardPoints(checkAnswer, confirmAnswer, possiblePoints) {
+    if (!(checkAnswer == 'incorrect' && confirmAnswer ==true)) {
+        //award points
+        let target = document.getElementById('score')
+        let currentScore = +(target.innerText)
+        currentScore += possiblePoints
+        target.innerText = currentScore
+    }else {
+        alert('No points awarded.')
+    }
+}
 
 //////////////////////////////////////////////////////////////////
 //fetch template
